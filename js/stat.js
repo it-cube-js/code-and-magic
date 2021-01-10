a19a95d68dc58d980442956d0dc2c15ae272abf4
Lenkeri <Lno4kamalt@yandex.ru>
2021-01-10 21:51:36 +0500

    начало облака. текст не появляется при fillText, а при strokeText - да

--- js/stat.js
+++ js/stat.js
@@ -1,21 +1,17 @@
 
-const names = [];
-const times = [];
 const cloud_width = 420;
 const cloud_height = 270;
 const cloud_x = 100;
 const cloud_y = 10;
-// const cloud_date = [420, 270, 100, 10];
-const canvas = document.getElementById('bar_chart');
-const ctx = canvas.getContext('2d');
-const renderStatistics = function(ctx, names, times){
+const cloud_date = [110, 20, 420, 270];
+
+window.renderStatistics = function(ctx, names, times){
+  ctx.fillStyle = 'rgba(0, 0, 0, 0.7)';
+  ctx.fillRect(cloud_date[0], cloud_date[1], cloud_date[2], cloud_date[3]);
+  ctx.fillStyle = 'white';
   ctx.fillRect(cloud_x, cloud_y, cloud_width, cloud_height);
-  ctx.fillStyle = "white";
+  ctx.font = "16px PT Mono";
+  ctx.strokeText("Ура вы победили!", 120, 30);
 
 };
 
-/* let renderStatistics = function(window){
-
-};
-
-*/
