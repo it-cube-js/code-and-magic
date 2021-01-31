806b65d6835d4d39739cc9a77014b02e82a463b8
Lenkeri <Lno4kamalt@yandex.ru>
2021-01-31 19:05:38 +0500

    начало

--- js/setup.js
+++ js/setup.js
@@ -1,2 +1,41 @@
 const setup = document.setup.classList.remove("hidden");
 
+const characterParameters = {
+  name: names,
+  surname: surnames,
+  coatColor: coatColors,
+  eyesColor: eyesColors
+}
+
+const names = [Иван, Хуан Себастьян, Мария, Кристоф, Виктор, Юлия, Люпита, Вашингтон]
+const surnames = []
+
+
+
+
+
+/*
+// const characterParameters = [names, surname, coatColor, eyesColor];
+
+const characterParameters = [names, coatColor, eyesColor];
+
+names = {
+  name: Иван,
+  name: Хуан Себастьян,
+  name: Мария,
+  name: Кристоф,
+  name: Виктор,
+  name: Юлия,
+  name: Люпита,
+  name: Вашингтон,
+
+  да Марья
+Верон
+Мирабелла
+Вальц
+Онопко
+Топольницкая
+Нионго
+Ирвинг
+
+};/*
