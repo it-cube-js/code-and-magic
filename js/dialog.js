a960b51565dfc62b04569389ad228e7e9ffe43ec
Lenkeri <lno4kamalt@yandex.ru>
2021-03-30 20:16:16 +0500

    перемещение окна

--- js/dialog.js
+++ js/dialog.js
@@ -1,42 +1,52 @@
 import {setup} from "./setup.js"
 const uploadMove = document.querySelector('.upload');
-// const setupTitleMove = document.querySelector('.setup-title')
+const setupTitleMove = document.querySelector('.setup-title')
 
-let mousedown = false;
-uploadMove.addEventListener('mousedown', function(evt){
-  mousedown = true;
-  let formX = evt.clientX - evt.offsetX
-  let formY = evt.clientY - evt.offsetY
-  console.log('setupWizardForm ' + formX + " X " + formY )
-  console.log('setup ' + setup.style.left + " X " + setup.style.top )
-
-console.log(evt)
-});
-
-
-
-// setupTitleMove.addEventListener('mousedown', function(evt){
+// uploadMove.addEventListener('mousedown', function(evt){
 //   mousedown = true;
+//   let formX = evt.clientX - evt.offsetX
+//   let formY = evt.clientY - evt.offsetY
+//   console.log('setupWizardForm ' + formX + " X " + formY )
+//   console.log('setup ' + setup.style.left + " X " + setup.style.top )
 
 // console.log(evt)
 // });
 
-uploadMove.addEventListener('mousemove', function(evt){
-if(mousedown){
-  let clientX = evt.clientX;
-  let clientY = evt.clientY;
-  let offsetX = evt.offsetX;
-  let offsetY = evt.offsetY;
 
-  let formX = evt.clientX - evt.offsetX
-  let formY = evt.clientY - evt.offsetY
 
-  // uploadMove.moveBy(clientX , clientY);
 
-}
+setupTitleMove.addEventListener('mousedown', function(evt) {
+  window.setup = setup;
+  const startX = evt.clientX;
+  const startY = evt.clientY;
+  const setupX = setup.offsetLeft;
+  const setupY = setup.offsetTop;
+  const onDocumentMouseMove = (evtMousemove) => {
+    const shiftX = evtMousemove.clientX - startX;
+    const shiftY = evtMousemove.clientY - startY;
+
+    console.log('shift: ', shiftX, shiftY);
+    // let stopX = evt.clientX;
+    // let stopY = evt.clientY;
+
+    // const dx = startX - stopX;
+    // const dy = startY - stopY;
+
+    console.log('setup ', setupX, setupY);
+
+    setup.style.left = setupX + shiftX + 'px';
+    setup.style.top = setupY + shiftY + 'px';
+
+    // startX = stopX;
+    // startY = stopY;
+  };
+
+  const onDocumentMouseup = () => {
+    document.removeEventListener('mousemove', onDocumentMouseMove);
+    document.removeEventListener('mouseup', onDocumentMouseup);
+  }
+
+  document.addEventListener('mousemove', onDocumentMouseMove);
+  document.addEventListener('mouseup', onDocumentMouseup);
 });
 
-uploadMove.addEventListener('mouseup', function(evt){
-  mousedown = false;
-
-});
